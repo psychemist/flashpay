f3054b0b6fcba5f82fbf136e225b968a7cc8e896
psychemist <ikeagudike@gmail.com>
2026-02-08 18:16:40 +0100

    fix: npm install & build errors

--- frontend/src/lib/abis.ts
+++ frontend/src/lib/abis.ts
@@ -153,6 +153,13 @@ export const OrgRegistryABI = [
         stateMutability: 'view',
     },
     {
+        type: 'function',
+        name: 'employeeToOrg',
+        inputs: [{ name: 'account', type: 'address' }],
+        outputs: [{ name: '', type: 'uint256' }],
+        stateMutability: 'view',
+    },
+    {
         type: 'event',
         name: 'OrganizationCreated',
         inputs: [
